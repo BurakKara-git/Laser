c437c83dcbf6786e85205825b024fc18d9601363
BurakKara-git <karaburak42@gmail.com>
2024-10-24 12:18:21 +0300

    minor changes

--- functions.py
+++ functions.py
@@ -632,7 +632,7 @@ def Fresnel(device_list: List[Device]):
             return math.sqrt(linear_speed**2 - r_velocity**2) / r
 
     # Calculate and Simulate Spirals
-    def calculate_path(RADIUS_LIST, R_VEL=0.02, LINEAR_VEL=100, view=False):
+    def calculate_path(RADIUS_LIST, R_VEL=0.02, LINEAR_VEL=100, view=False): 
         if view:
             screen = Screen()
             WIDTH, HEIGHT = screen.window_width(), screen.window_height()
@@ -684,13 +684,15 @@ def Fresnel(device_list: List[Device]):
             z1_index = RADIUS_LIST[i][0]
             z2_index = RADIUS_LIST[i][1]
 
+            # Get Z values in mm
             z1_diff = data[z1_index][1]
             z2_diff = data[z2_index][1]
-
+            
+            # Calculate Focus Z
             z1 = constants.INITIAL_Z - z1_diff
             z2 = constants.INITIAL_Z - z2_diff
 
-            # Get R Positions
+            # Get R Positions in mm
             r1 = RADIUS_LIST[i][0] / 1000
             r2 = RADIUS_LIST[i][1] / 1000
 
@@ -705,7 +707,7 @@ def Fresnel(device_list: List[Device]):
         return (points, thetas, theta_vels, rings)
 
     R_VEL = 0.05
-    LINEAR_VEL = 30
+    LINEAR_VEL = 40
     points, thetas, theta_vels, rings = calculate_path(
         RADIUS_LIST=constants.RADIUS_LIST,
         R_VEL=R_VEL,
@@ -718,21 +720,21 @@ def Fresnel(device_list: List[Device]):
     for point in points:
         r.append(point.r / 1000 + x_offset)
 
-    threads = []
     start = time.time()
     for i in range(len(rings)):
         ring = rings[i]
         x1 = ring.r1 + x_offset
         x2 = ring.r2 + x_offset
-        elapsed_time = (x2 - x1) / R_VEL
+        w = ring.w2
+        elapsed_time = abs(x2 - x1) / R_VEL
         print(
-            "Time = {}, R1 = {}, R2 = {}, Angular Velocity = {}".format(
-                elapsed_time, x1, x2, ring.w1
+            "Time = {}, R1 = {}, R2 = {}, Angular Velocity = {}, Z = {}".format(
+                elapsed_time, x1, x2, w, ring.z1
             )
         )
 
         # Start Rotational Movement
-        device.axisrot.move_velocity(ring.w2, Units.ANGULAR_VELOCITY_RADIANS_PER_SECOND)
+        device.axisrot.move_velocity(w, Units.ANGULAR_VELOCITY_RADIANS_PER_SECOND)
 
         # Go to Rings' X and Z Positions
         device.axisx.move_absolute(position=x1, unit=Units.LENGTH_MILLIMETRES)
@@ -747,7 +749,7 @@ def Fresnel(device_list: List[Device]):
             Units.VELOCITY_MILLIMETRES_PER_SECOND,
         )
 
-        # Set Z-Axis to Max Position (Unfocus)
+        # Set Z-Axis to Max Position (Unfocus) Will be changed to arduino switch
         device.axisz.move_absolute(constants.Z_MAX, unit=Units.LENGTH_MILLIMETRES)
 
         # Stop Rotational Movement
@@ -755,6 +757,7 @@ def Fresnel(device_list: List[Device]):
 
     end = time.time()
     print("Elapsed Tİme = {}".format(end - start))
+    device.extract_axes()
 
 
 # GCode(In Dev)
